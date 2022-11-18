1c76d901a1a40270decaac281a6f7de4f12bee25
fvill063 <frankvillegas808@gmail.com>
2022-11-18 14:07:43 -0700

    new changes

---
+++ sites.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require("path");
+const app = express();
+const port = 6600;
+
+app.use(express.static(path.join(__dirname, "static")));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+// app.post('/', function(req, res) {
+//   res.sendFile(__dirname + "/index.html")
+// });
+
+app.listen(port, function() {
+  console.log("listening to port 6600");
+})
